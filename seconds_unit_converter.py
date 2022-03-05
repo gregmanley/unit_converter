2c150dc43cc13f80f97fb1cb1d15956bd759fa35
Greg <g.y.manley@gmail.com>
2022-03-05 15:08:05 -0800

    Allowing for multiple choice of starting unit

--- seconds_unit_converter.py
+++ seconds_unit_converter.py
@@ -62,11 +62,28 @@ validate_user_input_starting_units()
 
 
 # Define function to convert from user input number and user input units to seconds
+## This is obviously really janky and not ooptimal. I bet there's a way to do a lookup or something
+
+def convert_to_answer(user_input_amount_int, user_input_starting_unit):
+    if user_input_starting_unit == "seconds":
+        return f"{user_input_amount_int} seconds equals {user_input_amount_int * seconds} seconds"
+    elif user_input_starting_unit == "minutes":
+        return f"{user_input_amount_int} minutes equals {user_input_amount_int * minutes} seconds"
+    elif user_input_starting_unit == "hours":
+        return f"{user_input_amount_int} hours equals {user_input_amount_int * hours} seconds"
+    elif user_input_starting_unit == "days":
+        return f"{user_input_amount_int} days equals {user_input_amount_int * days} seconds"
+    elif user_input_starting_unit == "weeks":
+        return f"{user_input_amount_int} weeks equals {user_input_amount_int * weeks} seconds"
+    elif user_input_starting_unit == "months":
+        return f"{user_input_amount_int} months equals {user_input_amount_int * months} seconds"
+    elif user_input_starting_unit == "years":
+        return f"{user_input_amount_int} years equals {user_input_amount_int * years} seconds"
+    elif user_input_starting_unit == "decades":
+        return f"{user_input_amount_int} decades equals {user_input_amount_int * decades} seconds"
+    else:
+        sys.exit(print(f"Unkown error"))
 
 
-def convert_to_answer(user_input_amount_int):
-    return f"{user_input_amount_int} days equals {user_input_amount_int * days} seconds"
-
-
-answer = convert_to_answer(user_input_amount_int)
+answer = convert_to_answer(user_input_amount_int, user_input_starting_unit)
 print(answer)
