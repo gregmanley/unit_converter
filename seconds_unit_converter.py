0953ffdea1e0146e4490ac9f93fab3cc9d51082f
Greg <g.y.manley@gmail.com>
2022-03-05 15:19:54 -0800

    Cleaning up todo

--- seconds_unit_converter.py
+++ seconds_unit_converter.py
@@ -2,13 +2,10 @@
 Building a unit converter that converts from any input unit into seconds
 
 To do:
-- Validate user input integer is > 0
-- Validate user input integer is not exactly 0
 - Fix the fact that months is assuming 4 weeks b/c I couldn't solve for floats
 - Combine both validations of user starting inputs
-- Accept user's input of desired amount and desired starting unit --> convert that to seconds
-    - Allow the user to do a multiple-choice select instead of making the user input the starting unit by manually typing it in
-    - Or, alternatively, can I make the initial input a seris of query params or just arguments passed?
+- Allow the user to do a multiple-choice select instead of making the user input the starting unit by manually typing it in. Maybe there is a module/builtin function I'm not aware of?
+    - Or, alternatively, can I make the initial input a series of query params or just arguments passed?
 - Build so that the user can choose their desired output units instead of only handling conversion to seconds
     - Import a dictionary of all of the base units so that I don't have to define each unit conversion as a variable?
 """
